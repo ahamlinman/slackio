58c1b08d6279200f3e7b76ec599cd3ee97a28ed1
Alex Hamlin <alex@alexhamlin.co>
2017-08-12 03:37:33 -0700

    Rename slackio.SlackIO to slackio.Client
    Get rid of some stuttering in the name. Not sure I'm 100% sold on
    "Client" but it is an improvement.

--- slackio.go
+++ slackio.go
@@ -6,11 +6,11 @@ import (
 	"github.com/nlopes/slack"
 )
 
-// SlackIO is a ReadWriteCloser that reads from and writes to a single Slack
+// Client is a ReadWriteCloser that reads from and writes to a single Slack
 // channel using the Slack real-time API. In this model, the content of the
 // main channel is represented as lines of text. Concepts like threads,
 // reactions, and even the senders of individual messages are entirely ignored.
-type SlackIO struct {
+type Client struct {
 	reader     io.ReadCloser
 	writer     io.WriteCloser
 	disconnect func() error
@@ -19,12 +19,12 @@ type SlackIO struct {
 // New returns a SlackIO for the given channel that uses the given Slack API
 // token. channel must be an ID rather than a channel name. This ID can be
 // obtained from the URL path when viewing Slack on the web.
-func New(token, channel string) *SlackIO {
+func New(token, channel string) *Client {
 	api := slack.New(token)
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 
-	return &SlackIO{
+	return &Client{
 		reader:     newRTMReader(rtm, channel),
 		writer:     newRTMWriter(rtm, channel),
 		disconnect: rtm.Disconnect,
@@ -35,7 +35,7 @@ func New(token, channel string) *SlackIO {
 // threads), buffered by line. Single messages will be terminated with an
 // appended newline. Messages with explicit line breaks are equivalent to
 // multiple single messages in succession.
-func (s *SlackIO) Read(p []byte) (int, error) {
+func (s *Client) Read(p []byte) (int, error) {
 	return s.reader.Read(p)
 }
 
@@ -43,14 +43,14 @@ func (s *SlackIO) Read(p []byte) (int, error) {
 // Slack channel. Every line is sent as an individual message, and no batching
 // of any kind is performed (though this is under consideration as a potential
 // improvement).
-func (s *SlackIO) Write(p []byte) (int, error) {
+func (s *Client) Write(p []byte) (int, error) {
 	return s.writer.Write(p)
 }
 
-// Close disconnects this SlackIO from the real-time API and shuts down
-// internal buffers. After calling Close, the next call to Read will result in
-// an EOF and the next call to Write will result in an error.
-func (s *SlackIO) Close() error {
+// Close disconnects this Client from the real-time API and shuts down internal
+// buffers. After calling Close, the next call to Read will result in an EOF
+// and the next call to Write will result in an error.
+func (s *Client) Close() error {
 	// These always return nil. See their respective comments.
 	s.reader.Close()
 	s.writer.Close()
