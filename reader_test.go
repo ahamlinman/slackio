b8263bb3e69d1c2024399d099590c2a11aad1b3f
Alex Hamlin <alex@alexhamlin.co>
2017-08-26 22:18:19 -0700

    Add unit tests for Reader
    Some additional unit testing for slackio. This covers nearly all of
    Reader's basic functionality.

---
+++ reader_test.go
@@ -0,0 +1,134 @@
+package slackio
+
+import (
+	"bytes"
+	"sync"
+	"testing"
+)
+
+type testReadClient struct {
+	messages []Message
+	closed   bool
+	wg       sync.WaitGroup
+}
+
+func (c *testReadClient) GetMessageStream() (<-chan *Message, chan<- struct{}) {
+	outCh, doneCh := make(chan *Message), make(chan struct{})
+
+	c.wg.Add(1)
+	go func() {
+		for _, m := range c.messages {
+			t := m
+			outCh <- &t
+		}
+		close(outCh)
+		c.wg.Done()
+	}()
+
+	c.wg.Add(1)
+	go func() {
+		<-doneCh
+		c.closed = true
+		c.wg.Done()
+	}()
+
+	return outCh, doneCh
+}
+
+func (c *testReadClient) wait() {
+	c.wg.Wait()
+}
+
+func TestReader(t *testing.T) {
+	client := &testReadClient{
+		messages: []Message{
+			{
+				Text:      "a message",
+				ChannelID: "C12345678",
+			},
+			{
+				Text:      "and another",
+				ChannelID: "C87654321",
+			},
+		},
+	}
+
+	r := &Reader{Client: client}
+	var readBytes [16]byte
+
+	expected := [][]byte{[]byte("a message\n"), []byte("and another\n")}
+
+	for _, e := range expected {
+		if _, err := r.Read(readBytes[:]); err != nil {
+			t.Fatalf("unexpected Reader error: %q", err.Error())
+		}
+
+		if !bytes.HasPrefix(readBytes[:], e) {
+			t.Fatalf("unexpected Reader output: %q (expected prefix %q)", readBytes, e)
+		}
+	}
+
+	if err := r.Close(); err != nil {
+		t.Fatalf("unexpected Reader error: %q", err.Error())
+	}
+
+	client.wait()
+	if !client.closed {
+		t.Fatal("Reader did not close message stream on Close")
+	}
+}
+
+func TestSingleChannelReader(t *testing.T) {
+	client := &testReadClient{
+		messages: []Message{
+			{
+				Text:      "a message",
+				ChannelID: "C12345678",
+			},
+			{
+				Text:      "not this one",
+				ChannelID: "C87654321",
+			},
+			{
+				Text:      "and another",
+				ChannelID: "C12345678",
+			},
+		},
+	}
+
+	r := &Reader{Client: client, SlackChannelID: "C12345678"}
+	var readBytes [16]byte
+
+	expected := [][]byte{[]byte("a message\n"), []byte("and another\n")}
+
+	for _, e := range expected {
+		if _, err := r.Read(readBytes[:]); err != nil {
+			t.Fatalf("unexpected Reader error: %q", err.Error())
+		}
+
+		if !bytes.HasPrefix(readBytes[:], e) {
+			t.Fatalf("unexpected Reader output: %q (expected prefix %q)", readBytes, e)
+		}
+	}
+
+	if err := r.Close(); err != nil {
+		t.Fatalf("unexpected Reader error: %q", err.Error())
+	}
+
+	client.wait()
+	if !client.closed {
+		t.Fatal("Reader did not close message stream on Close")
+	}
+}
+
+func TestReaderRequiresClient(t *testing.T) {
+	defer func() {
+		if err := recover(); err == nil {
+			t.Fatal("Reader did not panic with no Client")
+		}
+	}()
+
+	r := &Reader{}
+	var readBytes [1]byte
+	r.Read(readBytes[:])
+}
